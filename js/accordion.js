62bd32fa40fe71cde0a8102434e5a6bc5fd40084
alereyes18 <reyesalejandra161@gmail.com>
2023-01-24 16:18:14 -0500

    Fixed accordion, wasn't working in react

--- js/accordion.js
+++ js/accordion.js
@@ -1,26 +1,28 @@
- 
+if (document.readyState != 'loading'){
+    init();
+}
+else {
+    document.addEventListener('DOMContentLoaded', () => {
+        init();
+    })
+}
 
- document.addEventListener('DOMContentLoaded', () => {
+function init(){
+    const accordions = document.getElementsByClassName ( 'accordion-title' );
+    console.log ( accordions );
 
-    console.log('test');
-    const accordions = document.getElementsByClassName('accordion-title');
-    console.log(accordions);
+    if (document.readyState !=='loading'){
+        [...accordions].forEach(( accordiontitle, index, array ) => {
+            console.log ( accordiontitle );
+            accordiontitle.addEventListener ( 'click', () => {
+                console.log ( 'clicked' );
+                const content = accordiontitle.parentElement.querySelectorAll ( '.accordion-content' );
+                console.log ( content );
+                content.forEach ( ( contentdiv, key, parent ) => {
+                    contentdiv.classList.toggle ( 'expanded' );
+                } );
+            } );
     
-    [...accordions].forEach((accordiontitle, index, array) => {
-        console.log(accordiontitle);
-        accordiontitle.addEventListener("click", () => {
-            console.log('clicked');
-            const content = accordiontitle.parentElement.querySelectorAll('.accordion-content');
-            console.log(content);
-            content.forEach((contentdiv, key, parent) => {
-                contentdiv.classList.toggle('expanded');
-            })
-            })
-     
-        })
-     })
-    
-
-
- 
- 
+        } );
+    }
+}
