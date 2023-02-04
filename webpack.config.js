eb3404512c8dab1512891710ba74eb5a89c7d0a5
alereyes18 <reyesalejandra161@gmail.com>
2023-02-03 21:47:52 -0500

    main.yaml will now compile webpack, check for changes, then commit changes and update the npm package
    check changes fixed
    
    Fixing main.yaml
    
    Fixing main.yaml
    
    Fixing main.yaml
    
    Fixing main.yaml
    
    Fixing main.yaml
    
    Fixing main.yaml
    
    Updating main.yaml, added node caching
    
    Updating main.yaml

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ const config = {
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [stylesHandler, "css-loader", "sass-loader"],
+        use: [stylesHandler, "sass-loader"],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
