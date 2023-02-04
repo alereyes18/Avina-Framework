202800aabc373de59e86ec186dcd72a4d3e743f0
alereyes18 <reyesalejandra161@gmail.com>
2023-02-04 10:43:20 -0500

    Created build script for webpack with specified config Created start script for serving webpack dist Swapped MiniCssExtractPlugin for style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,9 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const isProduction = process.env.NODE_ENV == "production";
+ const isProduction = process.env.NODE_ENV == "production";
 
-const stylesHandler = MiniCssExtractPlugin.loader;
-
+ 
 const config = {
    devServer: {
     // Can be omitted unless you are using 'docker' 
@@ -15,13 +13,17 @@ const config = {
     avina : "./src/index.js",
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-  },
+    path: path.resolve(__dirname, "./dist"),
+    filename:"avina.js",
+    library: "AvinaFramework",
+    libraryTarget:"umd",
+    globalObject:"this",
+    umdNamedDefine:true
+  }, 
   plugins: [
 
 
-    new MiniCssExtractPlugin(),
-
+ 
     // Add your plugins here
     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
@@ -33,11 +35,11 @@ const config = {
       },
       {
         test: /\.css$/i,
-        use: [stylesHandler, "css-loader"],
+        use: [ "style-loader", "css-loader"],
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [stylesHandler, "css-loader", "sass-loader"],
+        use: [ "css-loader", "sass-loader"],
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
